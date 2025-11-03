e8c6b943e49818fc3438383961cfa029acdaf4ac
DevPeng1127 <pengveloper1127@gmail.com>
2025-11-03 16:06:13 +0900

    test :  백엔드 postgreSQL 연결된 기본 셋팅 완료

---
+++ src/main/java/run/prizm/chat_translate_demo/ChatTranslateDemoApplication.java
@@ -0,0 +1,13 @@
+package run.prizm.chat_translate_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatTranslateDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatTranslateDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/run/prizm/chat_translate_demo/ChatTranslateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package run.prizm.chat_translate_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatTranslateDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
