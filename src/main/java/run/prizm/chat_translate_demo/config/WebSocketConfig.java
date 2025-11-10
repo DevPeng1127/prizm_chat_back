d8dbe33bcde9497dca3695444522781984fd8325
DevPeng1127 <pengveloper1127@gmail.com>
2025-11-10 15:54:21 +0900

    fix : rabbitMQ를 브로커로 사용하도록 변경

--- src/main/java/run/prizm/chat_translate_demo/config/WebSocketConfig.java
+++ src/main/java/run/prizm/chat_translate_demo/config/WebSocketConfig.java
@@ -13,10 +13,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         // ✅ STOMP 브로커 사용 (RabbitMQ)
         //    /topic, /queue prefix를 사용하는 메시지를 브로커로 라우팅
         registry.enableStompBrokerRelay("/exchange","/topic", "/queue")
-                .setRelayHost("localhost") // application.yml에서 설정한 값과 동일하게
+                .setRelayHost("192.168.0.221") // application.yml에서 설정한 값과 동일하게
                 .setRelayPort(61613)       // RabbitMQ STOMP 기본 포트
                 .setClientLogin("admin")   // application.yml에서 설정한 값
-                .setClientPasscode("1234");
+                .setClientPasscode("1234")
+                .setSystemLogin("admin")
+                .setSystemPasscode("1234");
 
         // ✅ 클라이언트가 publish할 때 사용할 prefix
         //    예: stompClient.publish({ destination: "/pub/chat.send", body: ... })
